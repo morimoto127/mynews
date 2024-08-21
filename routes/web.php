185321a53753c889ee72bafe0811200a00b2b9bf
erina <elmo.01271987@gmail.com>
2024-08-21 06:23:14 +0000

    課題提出２

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+use App\Http\Controllers\Admin\NewsController;
+Route::controller(NewsController::class)->prefix('admin')->group(function(){
+    Route::get('news/create','add');
+});
+
+//問題（３）
+// Route::get('/','AAAController@bbb');
+
+//問題（４）
+use App\Http\Controllers\Admin\ProfileController;
+Route::controller(ProfileController::class)->prefix('admin')->group(function()
+{
+    Route::get('profile/create','add');
+    Route::get('profile/edit','edit');
+});
